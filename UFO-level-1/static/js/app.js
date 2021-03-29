b57cc4ff0ede6db6b14a5701ec6761bd2b14ed11
Alex Martinez <alexmartinez1745@gmail.com>
2021-03-29 18:39:37 -0400

    iterating through table data in console

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,10 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+// let date = "1/11/2010";
+
+// tableData.filter(dates) => {
+//     return dates.datetime === date;
+// }
