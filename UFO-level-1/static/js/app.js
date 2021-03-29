292417796777ec23823ac8ff21348e540ce220db
Alex Martinez <alexmartinez1745@gmail.com>
2021-03-29 18:50:45 -0400

    appending table data to html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,7 +2,20 @@
 var tableData = data;
 console.log(tableData);
 
+// Reference to the table body
+let tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+tableData.forEach((sightings) => {
+    let row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
+
+
 // let date = "1/11/2010";
 
 // tableData.filter(dates) => {
